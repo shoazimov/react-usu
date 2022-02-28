d9c62f617a215687790c78253ff84627ff68e6fa
Muhammadrizo <73033375+shoazimov@users.noreply.github.com>
2022-02-28 21:34:59 +0500

    Update ProductList.jsx

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import "./productList.css";
-import Product from "../product/Product";
 import usu1 from "../../usu-images/usu1.jpg";
 import usu2 from "../../usu-images/usu2.jpg";
 import usu3 from "../../usu-images/usu3.jpg";
 import usu4 from "../../usu-images/usu4.jpg";
-import usu5 from "../../usu-images/usu5.jpg";
+import usu8 from "../../usu-images/usu8.jpg";
 import usu7 from "../../usu-images/usu7.jpg";
+import Product from "../product/Product";
 
 const ProductList = () => {
   return (
-    <div className="pl">
-      <div className="pl-texts">
-        <h1 className="pl-title">Process</h1>
-        <p className="pl-desc">There are some highlights from 𝕌𝕊𝕌</p>
+    <>
+      <div className="pl">
+        <div className="pl-texts">
+          <h1 className="pl-title">Process</h1>
+          <p className="pl-desc">
+            There are some highlights from Speaking Club
+          </p>
+        </div>
+        <div className="pl-list">
+          <Product img={usu1} />
+          <Product img={usu2} />
+          <Product img={usu3} />
+          <Product img={usu7} />
+          <Product img={usu4} />
+          <Product img={usu8} />
+        </div>
       </div>
-      <div className="pl-list">
-        <Product img={usu1} />
-        <Product img={usu2} />
-        <Product img={usu3} />
-        <Product img={usu7} />
-        <Product img={usu4} />
-        <Product img={usu5} />
-      </div>
-    </div>
+    </>
   );
 };
 
