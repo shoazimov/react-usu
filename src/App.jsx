cdc1f102f78d12a9dfceb7a179a34443f394ed38
Muhammadrizo <73033375+shoazimov@users.noreply.github.com>
2022-03-24 19:24:01 +0500

    adding Mentors Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import About from "./components/about/About";
 import Intro from "./components/intro/Intro";
 import Contact from "./components/contact/Contact";
 import ProductList from "./components/productList/ProductList";
+import Mentors from './components/mentors/Mentors';
 
 const App = () => {
   return (
     <div>
       <Intro />
+      <Mentors />
       <About />
       <ProductList />
       <Contact />
